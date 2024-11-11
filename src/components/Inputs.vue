d99830047b2263b48cafc68ff380cf8363045b48
MestreSol <mb.muitobosta@gmail.com>
2024-11-11 18:25:23 -0300

    Add Functions

---
+++ src/components/Inputs.vue
@@ -0,0 +1,231 @@
+<script>
+
+    let isShift = false;
+    let isAlpha = false;
+    
+    function Shift(){
+        isShift = !isShift;
+    }
+
+    function Alpha(){
+        isAlpha = !isAlpha;
+    }
+
+    function Menu(){
+        
+    }
+
+    function On(){
+        return null;
+    }
+    
+    function options(){
+        return null;
+    }
+
+    function calc(){
+        return null;
+    }
+
+    function integrals(){
+        return null;
+    }
+
+    function x(){
+        return null;
+    }
+
+    function divide(){
+        return null;
+    }
+
+    function rad(){
+        return null;
+    }
+
+    function elevado(){
+        return null;
+    }
+
+    function elevado2(){
+        return null;
+    }  
+
+    function log(){
+        return null;
+    }
+
+    function Ln(){
+        return null;
+    }   
+
+    function negative(){
+        return null;
+    }
+
+    function line()
+    {
+        return null;
+    }
+
+    function elevadon1(){
+        return null;
+    }
+
+    function sin(){
+        return null;
+    }
+
+    function cos(){
+        return null;
+    }
+
+    function tan(){
+        return null;
+    }   
+
+    function sto(){
+        return null;
+    }
+
+    function eng(){
+        return null;
+    }
+
+    function parentes(){
+        return null;
+    }
+    function parentes2(){
+        return null;
+    }
+
+    function S_D(){
+        return null;
+    }
+
+    function MPluz(){
+        return null;
+    }
+
+    function seven(){
+        return null;
+    }
+
+    function eith(){
+        return null;
+    }  
+
+    function nine(){
+        return null;
+    }
+
+    function four(){
+        return null;
+    }
+
+    function five(){
+        return null;
+    }
+
+    function six(){
+        return null;
+    }
+
+    function one(){
+        return null;
+    }
+
+    function two(){
+        return null;
+    }
+
+    function three(){
+        return null;
+    }
+
+    function zero(){
+        return null;
+    }
+
+    function point(){
+        return null;
+    }
+
+    function cientificNotation(){
+        return null;
+    }
+
+    function Ans(){
+        return null;
+    }
+
+    function DEL(){
+        return null;
+    }
+
+    function AC(){
+        return null;
+    }
+
+    function equal(){
+        return null;
+    }
+
+    function Plus(){
+        return null;
+    }
+
+    function Menus(){
+        return null;
+    }
+
+    function Mult(){
+        return null;
+    }
+
+    function Div(){
+        return null;
+    }
+
+    function Setup(){
+        return null;
+    }
+
+    function QR(){
+        return null;
+    }
+
+    function Solver(){
+        return null;
+    }   
+
+    function dDx(){
+        return null;
+    }
+
+    function dots(){
+        return null;
+    }
+
+    function somatoria(){
+        return null;
+    }
+
+    function divideMultiplier(){
+        return null;
+    }
+
+    function sqrtCube(){
+        return null;
+    }
+
+    function xcube(){
+        return null;
+    }
+
+    function xquad(){
+        return null;
+    }
+
+    
+</script>
---
+++ src/components/LCD.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="lcd">
+        <p>{{ displayValue }}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'LCD',
+    props: {
+        displayValue: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.lcd {
+    background-color: #000;
+    color: #0f0;
+    font-family: 'Digital-7', sans-serif;
+    font-size: 2em;
+    padding: 10px;
+    border: 2px solid #333;
+    border-radius: 5px;
+    text-align: right;
+}
+</style>
