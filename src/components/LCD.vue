fd1b44ac389b893a9b40878d4c60bbad9227f293
MestreSol <mb.muitobosta@gmail.com>
2024-11-12 15:24:50 -0300

    🔖 Update calculator layout and functionality with new components and styles

--- src/components/LCD.vue
+++ src/components/LCD.vue
@@ -1,8 +1,41 @@
 <script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  display: {
+    type: String,
+    required: true
+  }
+});
 </script>
 
 <template>
+
     <div class="lcd">
-        <h1 class="green">LCD</h1>
+        <h1 class="green">{{ display }}</h1>
     </div>
 </template>
+
+<style scoped>
+
+.lcd {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: #f2f2f2;
+    border: 1px solid #ccc;
+    border-radius: 10px;
+    width: 250px;
+    height: 100px;
+    margin: 0 auto;
+}
+
+h1{
+    font-weight: 200;
+    font-size: 2.2ch;
+    position: relative;
+    top: -10px;
+    font-family: "Silkscreen", sans-serif;
+
+}
+</style>
